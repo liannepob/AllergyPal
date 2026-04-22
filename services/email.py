7a5e6e0b46ee414dad99aa6b8f392b709e125de1
Lianne Poblador <lianne.poblador@gmail.com>
2026-04-22 11:54:37 -0400

    updated email on email.py

--- services/email.py
+++ services/email.py
@@ -5,7 +5,7 @@ import os
 def send_reset_email(to_email, reset_link):
     message = Mail(
         from_email="noreply@allergypal.com",          # what email sends from?
-        to_emails=to_email,           # who receives it?
+        to_emails="liannepoblador@gmail.com",           # who receives it?
         subject= "AllergyPal: Reset Password",             # what's the subject line?
         html_content=f'<p>Hi AllergyPal user! Click <a href="{reset_link}">here</a> to reset your password. Link expires in 30 minutes.</p>'    )
     
